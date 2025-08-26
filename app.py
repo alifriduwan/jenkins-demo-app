5048ddfd1bff4b5753df004daced1c12ffb1154e
Alifriduwan <6510110565@email.psu.ac.th>
2025-08-26 12:38:20 +0700

    exercise 2

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ def hello():
     return "Hello 6510110565 from Jenkins Docker Pipeline Demo!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8081)
