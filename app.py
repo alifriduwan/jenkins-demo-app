b15b62a6234591c27c1f6f831f08c8dc68f7958f
Alifriduwan <6510110565@email.psu.ac.th>
2025-08-26 12:32:27 +0700

    exercise 1

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Jenkins Docker Pipeline Demo!"
+    return "Hello 6510110565 from Jenkins Docker Pipeline Demo!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
